944d1c7e1e12feeb02a2e0006ede6ca5a93fd48b
McFree <seda15678@gmail.com>
2018-02-24 17:04:51 +0300

    Увеличение очереди

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ var spawns_proc = require('spawn.proc');
 var memory_checked = require('memory.checked');
 
 module.exports.loop = function() {
-    spawns_proc.processing();
     memory_checked.checkMemory();
+    spawns_proc.processing();
 }
