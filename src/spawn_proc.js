3f370a2b738ee0fc62e8a0ddd16fe9918197ae11
McFree <seda15678@gmail.com>
2018-02-24 14:42:43 +0300

    Корректировки ошибок

--- src/spawn_proc.js
+++ src/spawn_proc.js
@@ -3,7 +3,7 @@ var Spawn_1 = require('spawn1_controller');
 module.exports = {
   processing : function() {
     if(Game.spawns.Spawn1) {
-      Spawn_1.processing(Spawn1);
+      Spawn_1.processing(Game.spawns.Spawn1);
     } 
   }
 };
