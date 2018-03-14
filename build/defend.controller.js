910dae1f8955bbc4ef4bd0d345d9084597908fde
Aleksandr Kusak <a.kusak@runetsoft.ru>
2018-03-14 20:10:14 +0300

    Фиксы

--- build/defend.controller.js
+++ build/defend.controller.js
@@ -1,12 +1,12 @@
-var HOSLILES = null;
+var HOSTILES = null;
 const CREEP_MOVE_ATACK       = {visualizePathStyle: {stroke: '#ee6a50'}};
 const CREEP_MOVE_LINE        = {visualizePathStyle: {stroke: '#ffffff'}};
 
 module.exports = {
   processing: function(room, hostles, solders, rangeds) {
-    HOSLILES = hostles;
+    HOSTILES = hostles;
     let roomName = room.name;
-    var username = HOSLILES[0].owner;
+    var username = HOSTILES[0].owner;
     Game.notify(`User ${username} spotted in room ${roomName}`);
     solders.forEach(creep => this.solderDefend(creep, false));
     rangeds.forEach(creep => this.solderDefend(creep, true));
@@ -14,7 +14,7 @@ module.exports = {
 
   solderDefend: function(creep, isRanged) {
     if(creep.room.name == creep.memory.target) {
-      target = creep.pos.findClosestByPath(HOSTLES[0]);
+      target = creep.pos.findClosestByPath(HOSTILES[0]);
       if(target) {
         if(isRanged) {
           if(creep.rangedAttack(target) == ERR_NOT_IN_RANGE){
---
+++ build/tower.controller.js
@@ -0,0 +1,38 @@
+const ROOM_STANDART   = 0;
+const ROOM_EVOLUTION  = 1;
+const ROOM_DEFEND     = 2;
+const ROOM_ATACK      = 3;
+
+module.exports = {
+  processing: function(ROOM_STATUS, hostles) {
+    var towers = room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
+
+    if (ROOM_STATUS == ROOM_DEFEND && hostles.length > 0) {
+        towers.forEach(tower => tower.attack(hostles[0]));
+    } else {
+        towers.forEach(tower => this.repairOrHeal(tower));
+    }
+  },
+
+  repairOrHeal: function(tower) {
+    var woundedCreep = tower.pos.findClosestByRange(FIND_CREEPS, { filter: function(creep) { 
+        return creep.hits < creep.hitsMax;
+      }
+    });
+    if(woundedCreep) {
+        if (Math.abs(tower.pos.x-woundedCreep.pos.x) < 20 && Math.abs(tower.pos.y-woundedCreep.pos.y) < 20) {
+            tower.heal(woundedCreep);
+        }
+    } else {
+        var needRepairStructure = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, { filter: function(obj) { 
+            return obj.hits < obj.hitsMax;
+          }
+        });
+        if(needRepairStructure) {
+            if (Math.abs(tower.pos.x-needRepairStructure.pos.x) < 20 && Math.abs(tower.pos.y-needRepairStructure.pos.y) < 20) {
+                tower.repair(needRepairStructure);
+            }
+        }
+    }
+  }
+}
