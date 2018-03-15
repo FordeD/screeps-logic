358302feacb8ed14838b0b590a97a3256fe1fc79
McFree <seda15678@gmail.com>
2018-03-15 03:38:34 +0300

    Правки

--- build/queue.controller.js
+++ build/queue.controller.js
@@ -2,49 +2,49 @@ module.exports = {
   addHarvester: function(roomName, creepLevel, spawnName) {
     if(SPAWN_QUEUE[roomName].length < SPAWN_QUEUE_MAX) {
       SPAWN_QUEUE[roomName].push({body: HARVESTER_BODY[creepLevel], memory: {role : ROLES.harvester, isTransfer : false, owner: spawnName }});
-      console.log("Add to queue " + spawnName + " a harvester. Queue: "+SPAWN_QUEUE[roomName].length);
+      notifier.infoNotify(spawnName, "Add to queue a harvester. Queue: "+SPAWN_QUEUE[roomName].length);
     }
   },
 
   addUpgrader: function(roomName, creepLevel, spawnName) {
     if(SPAWN_QUEUE[roomName].length < SPAWN_QUEUE_MAX) {
       SPAWN_QUEUE[roomName].push({body: CL_UPGRADER_BODY[creepLevel], memory: {role : ROLES.upgrader, isTransfer : false, owner: spawnName }});
-      console.log("Add to queue " + spawnName + " a cl_upgrader. Queue: "+SPAWN_QUEUE[roomName].length);
+      notifier.infoNotify(spawnName, "Add to queue a cl_upgrader. Queue: "+SPAWN_QUEUE[roomName].length);
     }
   },
 
   addBuilder: function(roomName, creepLevel, spawnName) {
     if(SPAWN_QUEUE[roomName].length < SPAWN_QUEUE_MAX) {
       SPAWN_QUEUE[roomName].push({body: EX_BUILDER_BODY[creepLevel], memory: {role : ROLES.builder, isTransfer : false, isBuilding : false, owner: spawnName }});
-      console.log("Add to queue " + spawnName + " a ex_builder. Queue: "+SPAWN_QUEUE[roomName].length);
+      notifier.infoNotify(spawnName, "Add to queue a ex_builder. Queue: "+SPAWN_QUEUE[roomName].length);
     }
   },
 
   addRepairer: function(roomName, creepLevel, spawnName) {
     if(SPAWN_QUEUE.length < SPAWN_QUEUE_MAX) {
       SPAWN_QUEUE[roomName].push({body: REPAIRER_BODY[creepLevel], memory: {role : ROLES.repairer, isTransfer : false, isRepair : false, target: roomName, owner: spawnName }});
-      console.log("Add to queue " + spawnName + " a repairer. Queue: "+SPAWN_QUEUE[roomName].length);
+      notifier.infoNotify(spawnName, "Add to queue a repairer. Queue: "+SPAWN_QUEUE[roomName].length);
     }
   },
 
   addSolder: function(roomName, creepLevel, spawnName) {
     if(SPAWN_QUEUE[roomName].length < SPAWN_QUEUE_MAX) {
       SPAWN_QUEUE[roomName].push({body: SOLDER_BODY[creepLevel], memory: {role : ROLES.solder, target: roomName, owner: spawnName }});
-      console.log("Add to queue " + spawnName + " a solder. Queue: "+SPAWN_QUEUE[roomName].length);
+      notifier.infoNotify(spawnName, "Add to queue a solder. Queue: "+SPAWN_QUEUE[roomName].length);
     }
   },
 
   addRanger: function(roomName, creepLevel, spawnName) {
     if(SPAWN_QUEUE[roomName].length < SPAWN_QUEUE_MAX) {
       SPAWN_QUEUE[roomName].push({body: RANGER_BODY[creepLevel], memory: {role : ROLES.ranger, target: roomName, owner: spawnName }});
-      console.log("Add to queue " + spawnName + " a ranger. Queue: "+SPAWN_QUEUE[roomName].length);
+      notifier.infoNotify(spawnName, "Add to queue a ranger. Queue: "+SPAWN_QUEUE[roomName].length);
     }
   },
 
   addHealer: function(roomName, creepLevel, spawnName) {
     if(SPAWN_QUEUE[roomName].length < SPAWN_QUEUE_MAX) {
       SPAWN_QUEUE[roomName].push({body: HEALER_BODY[creepLevel], memory: {role : ROLES.healer, target: roomName, owner: spawnName }});
-      console.log("Add to queue " + spawnName + " a healer. Queue: "+SPAWN_QUEUE[roomName].length);
+      notifier.infoNotify(spawnName, "Add to queue a healer. Queue: "+SPAWN_QUEUE[roomName].length);
     }
   },
 
@@ -53,9 +53,9 @@ module.exports = {
     if (!creeps.length || harvesters.length == 0 && spawnRoom.energyAvailable < MIN_SPAWN_ENERGY[creepLevel]) {
       res = spawnObj.createCreep(HARVESTER_BODY_ECO, null, {role : ROLES.harvester, isTransfer : false, sourceId : null, owner: spawnName });
       if(_.isString(res)) {
-        console.log("Creating in " + spawnName + " a ECO harvester '" + res + "'");
+        notifier.infoNotify(spawnName, "Creating a ECO harvester '" + res + "'");
       } else {
-        console.log("ECO harvester spawn in " + spawnName + " error: " + res);
+        notifier.wrongNotify(spawnName, "ECO harvester spawn error: " + res);
       }
       SPAWN_QUEUE[roomName] = [];
     } else {
@@ -90,9 +90,9 @@ module.exports = {
     var res;
     res = spawnObj.createCreep(creep.body, null, creep.memory);
     if(_.isString(res)) {
-      console.log("Creating a " + creep.memory.role + " '" + res + "' was started. Queue: "+SPAWN_QUEUE[roomName].length);
+      notifier.infoNotify(creep.memory.role, "Creating '" + res + "' was started. Queue: "+SPAWN_QUEUE[spawnObj.room.name].length);
     } else {
-      console.log(creep.memory.role + " spawn error: " + res);
+      notifier.wrongNotify(creep.memory.role, " spawn error: " + res);
     }
   },
 };
