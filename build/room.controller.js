3157d4b2edfe75b80404503247ecb4942965817c
McFree <seda15678@gmail.com>
2018-03-17 19:38:57 +0300

    Правки

--- build/room.controller.js
+++ build/room.controller.js
@@ -404,7 +404,7 @@ module.exports = {
         creep.moveTo(route);
       } else {
         if(creep.room.controller && !creep.room.controller.my) {
-          if(creep.attackController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+          if(creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
               creep.moveTo(creep.room.controller);
           }
         }
