6979b2e9b046dd5bc5a829fe363b381b2f8b074e
McFree <seda15678@gmail.com>
2018-03-17 20:55:34 +0300

    Изменение

--- build/room.controller.js
+++ build/room.controller.js
@@ -694,7 +694,7 @@ module.exports = {
 
     CREEPS = _.filter(Game.creeps, (creep) => creep.memory.owner == SPAWN_NAME);
     COMBAT_CREEPS = _.filter(CREEPS, (creep) => creep.memory.role == ROLES.solder || creep.memory.role == ROLES.ranger);
-    WORK_CREEPS = _.filter(CREEPS, (creep) => creep.memory.role != ROLES.solder || creep.memory.role != ROLES.ranger || creep.memory.role != ROLES.repairer);
+    WORK_CREEPS = _.filter(CREEPS, (creep) => creep.memory.role != ROLES.solder && creep.memory.role != ROLES.ranger && creep.memory.role != ROLES.repairer);
     let hostiles = SPAWN_ROOM.find(FIND_HOSTILE_CREEPS);
     if (!HOSTILES) {
       HOSTILES = [];
