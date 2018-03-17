dca38f2b815330546290304c42e9c260eec53d7c
McFree <seda15678@gmail.com>
2018-03-17 19:06:25 +0300

    Отчистка памяти флагов

--- build/memory.checked.js
+++ build/memory.checked.js
@@ -1,12 +1,22 @@
 module.exports = {
   checkMemory : function() {
     this.checkCreepsMemory();
+    this.checkFlagsMemory();
   },
 
   checkCreepsMemory : function() {
     for(var name in Memory.creeps) {
       if(!Game.creeps[name]) {
         console.log('Remove empty creep data '+name);
+        delete Memory.creeps[name];
+      }   
+    }
+  },
+
+  checkFlagsMemory : function() {
+    for(var name in Memory.flags) {
+      if(!Game.flags[name]) {
+        console.log('Remove empty flag data '+name);
         delete Memory.creeps[name];
       }   
     }
