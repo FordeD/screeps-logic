5e21dbb8d8ea87467250e76f755d90589a7726d5
McFree <seda15678@gmail.com>
2018-03-19 22:36:58 +0300

    Правки приоритетов

--- build/room.controller.js
+++ build/room.controller.js
@@ -106,14 +106,14 @@ module.exports = {
         }});
 
         var withoutEnergyStructures = [
-          SPAWN_OBJ.energy < SPAWN_OBJ.energyCapacity ? SPAWN_OBJ : false,
-          CREEPS.length >= SPAWN_QUEUE_MAX-2 ? store : extension,
           creep.pos.findClosestByRange(FIND_STRUCTURES, { filter: (obj) => { 
             if(obj.structureType == STRUCTURE_TOWER) {
               return obj.energy < 500;
             }
             return false;
           }}),
+          SPAWN_OBJ.energy < SPAWN_OBJ.energyCapacity ? SPAWN_OBJ : false,
+          CREEPS.length >= SPAWN_QUEUE_MAX-2 ? store : extension,
           CREEPS.length >= SPAWN_QUEUE_MAX-2 ? extension : store
         ];
 
